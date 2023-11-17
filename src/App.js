e848fde8f21871ab60b8dfb49917207d3844c792
ThavaneshR <thavaneshr5@gmail.com>
2023-11-17 18:53:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import QuizRoute from './components/QuizRoutes';
+import { AuthContextProvider } from './context/AuthContext';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "primeicons/primeicons.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthContextProvider>
+      <QuizRoute/>
+      </AuthContextProvider>
+      
     </div>
   );
 }
